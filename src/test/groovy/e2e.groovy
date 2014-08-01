a60aa04c9a230a7607b9d7da2ec775804cc8a34b
emulleo <leopoldo.muller@ericsson.com>
2014-08-01 13:32:45 +0100

    NEW FORMAT version specification

--- src/test/groovy/e2e.groovy
+++ src/test/groovy/e2e.groovy
@@ -79,12 +79,13 @@ class EndToEndTest {
 
             def response = client.get(
                 path: '/rest-api',
-                headers: [ 'Accept': 'application/json' ],
-                query: ['language': language]
+                headers: [ 'Accept': 'application/x.testapp.20140801+json' ],
+                query: ['language': language],
+                contentType: 'application/json'
             )
 
             assert response.status == 200
-            assert response.contentType == 'application/json'
+            assert response.contentType == 'application/x.testapp.20140801+json'
 
             return response.data.msg
         }
